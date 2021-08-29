f7262f5b8280c537fe1692caaac86271a872e6ed
SangYoon Kim <ndkim11@naver.com>
2021-08-29 22:16:57 +0900

    ts.transition parameter define comment

--- env.py
+++ env.py
@@ -19,53 +19,55 @@ from tf_agents.trajectories import time_step as ts
 
 tf.compat.v1.enable_v2_behavior()
 
+
 class EnduranceEnv(py_environment.PyEnvironment):
-  def __init__(self):
-    # action: front, turn left, turn right
-    self._action_spec = array_spec.BoundedArraySpec(
-        shape=(), dtype=np.int32, minimum=0, maximum=2, name='action')
-    # observation: 3 sensors (left, center, right), senser values are divided by 20 levels.
-    self._observation_spec = array_spec.BoundedArraySpec(
-        shape=(3,), dtype=np.int32, minimum=0, maximum=20, name='observation')
-    self._state = 0
-    self._episode_ended = False
+    def __init__(self):
+        # action: front, turn left, turn right
+        self._action_spec = array_spec.BoundedArraySpec(
+            shape=(), dtype=np.int32, minimum=0, maximum=2, name='action')
+        # observation: 3 sensors (left, center, right), senser values are divided by 20 levels.
+        self._observation_spec = array_spec.BoundedArraySpec(
+            shape=(3,), dtype=np.int32, minimum=0, maximum=20, name='observation')
+        self._state = 0
+        self._episode_ended = False
 
-  def action_spec(self):
-    return self._action_spec
+    def action_spec(self):
+        return self._action_spec
 
-  def observation_spec(self):
-    return self._observation_spec
+    def observation_spec(self):
+        return self._observation_spec
 
-  def _reset(self):
-    self._state = 0
-    self._episode_ended = False
-    return ts.restart(np.array([self._state], dtype=np.int32))
+    def _reset(self):
+        self._state = 0
+        self._episode_ended = False
+        return ts.restart(np.array([self._state], dtype=np.int32))
 
-  def _step(self, action):
+    def _step(self, action):
 
-    if self._episode_ended:
-      # The last action ended the episode. Ignore the current action and start
-      # a new episode.
-      return self.reset()
+        if self._episode_ended:
+            # The last action ended the episode. Ignore the current action and start
+            # a new episode.
+            return self.reset()
 
-    # Make sure episodes don't go on forever.
-    if action == 0:
-      connector.moveForward()
-      self._state = action
-    elif action == 1:
-      connector.turnLeft()
-      self._state = action
-    elif action == 2:
-      connector.turnRight()
-      self._state = action
-    else:
-      raise ValueError('`action` should be 0 or 1.')
+        # Make sure episodes don't go on forever.
+        if action == 0:
+            connector.moveForward()
+            self._state = action
+        elif action == 1:
+            connector.turnLeft()
+            self._state = action
+        elif action == 2:
+            connector.turnRight()
+            self._state = action
+        else:
+            raise ValueError('`action` should be 0 or 1 or 2')
 
-    if self._episode_ended or connector.getGoalDistance() < 1:
-      # found goal
-      self._episode_ended = True
-      return ts.termination(np.array([self._state], dtype=np.int32), reward=10)
-    else:
-      # not found goal
-      return ts.transition(
-          np.array([self._state], dtype=np.int32), reward=-0.3, discount=-1/connector.getGoalDistance())
+        if self._episode_ended or connector.getGoalDistance() < 1:
+            # found goal
+            self._episode_ended = True
+            return ts.termination(np.array([self._state], dtype=np.int32), reward=10)
+        else:
+            # not found goal
+            return ts.transition(
+                np.array([self._state], dtype=np.int32), reward=-0.3, discount=-1/connector.getGoalDistance())
+            # transition(observation,reward,discount,outer_dims)

5c372cb0cb70eb931abfa8f667d78645520b3fd6
Jeonghyun Kim <kimdictor@gmail.com>
2021-08-29 21:13:23 +0900

    Add import and spec definition

--- env.py
+++ env.py
@@ -1,11 +1,32 @@
 from airsimConnector import connector
 
-class CardGameEnv(py_environment.PyEnvironment):
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import tensorflow as tf
+import numpy as np
+
+from tf_agents.environments import py_environment
+from tf_agents.environments import tf_environment
+from tf_agents.environments import tf_py_environment
+from tf_agents.environments import utils
+from tf_agents.specs import array_spec
+from tf_agents.environments import wrappers
+from tf_agents.environments import suite_gym
+from tf_agents.trajectories import time_step as ts
+
+tf.compat.v1.enable_v2_behavior()
+
+class EnduranceEnv(py_environment.PyEnvironment):
   def __init__(self):
+    # action: front, turn left, turn right
     self._action_spec = array_spec.BoundedArraySpec(
-        shape=(), dtype=np.int32, minimum=0, maximum=1, name='action')
+        shape=(), dtype=np.int32, minimum=0, maximum=2, name='action')
+    # observation: 3 sensors (left, center, right), senser values are divided by 20 levels.
     self._observation_spec = array_spec.BoundedArraySpec(
-        shape=(1,), dtype=np.int32, minimum=0, name='observation')
+        shape=(3,), dtype=np.int32, minimum=0, maximum=20, name='observation')
     self._state = 0
     self._episode_ended = False
 

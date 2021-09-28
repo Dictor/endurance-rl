93cbb58e0a6025c997d4e16ee477aee8c1d2c5ee
Jeonghyun Kim <kimdictor@gmail.com>
2021-09-28 22:33:55 +0900

    expand step limit to 250

--- env.py
+++ env.py
@@ -80,7 +80,7 @@ class EnduranceEnv(py_environment.PyEnvironment):
                 print("[EnduranceEnv] termination: colided")
                 return ts.termination(np.array(self._state, dtype=np.int), -10)
 
-            if self.step_count > 200:
+            if self.step_count > 250:
                 self._episode_ended = True
                 print("[EnduranceEnv] termination: step limit over")
                 return ts.termination(np.array(self._state, dtype=np.int), -10)

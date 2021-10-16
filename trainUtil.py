22da7da4938b9bac5d899ef7a70a796de08e2a50
Jeonghyun Kim <kimdictor@gmail.com>
2021-10-16 19:58:06 +0900

    Add length, success rate displaying

--- trainUtil.py
+++ trainUtil.py
@@ -17,6 +17,8 @@ def collect_data(env, policy, buffer, steps):
 def compute_avg_return(environment, policy, num_episodes=10):
     print("[compute_avg_return] start, episode num = {0}".format(num_episodes))
     total_return = 0.0
+    total_episode_length = 0
+    success_episode = 0
     for e in range(num_episodes):
         time_step = environment.reset()
         episode_return = 0.0
@@ -27,8 +29,14 @@ def compute_avg_return(environment, policy, num_episodes=10):
             time_step = environment.step(action_step.action)
             episode_return += time_step.reward
             i = i + 1
+        total_episode_length += i
         total_return += episode_return
+        if episode_return > 0:
+            success_episode += 1
     avg_return = total_return / num_episodes
+    avg_episode_length = total_episode_length / num_episodes
+    success_rate = success_episode / num_episodes
     rtn = avg_return.numpy()[0]
-    print("[compute_avg_return] complete, avg_rtn={0}".format(rtn))
+    print(
+        "[compute_avg_return] avg_rtn={0} avg_len={1:.2f} success_rate={2:.2f}".format(rtn, avg_episode_length, success_rate))
     return rtn
